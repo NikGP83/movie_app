df5df65ef4795822ca5599dbf871ce2d35531480
Nik G <nik.golovin83@gmail.com>
2023-08-11 10:15:55 +0600

    переделал header, теперь на каждой странице поведение кнопок логичнее

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,37 +1,75 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './header.scss';
 import { APP_ROUTES } from '../../const/const';
 
 function Header() {
-
+  const { pathname } = useLocation();
   return (
     <header className="header">
       <nav className="header-nav">
         <div className="container">
           <ul className="header-list">
-            <li className="header-item">
-              <a href="#movies" className="header-link">
-                <span className="header-text">Movies</span>
-              </a>
-            </li>
-            <li className="header-item">
-              <Link to={APP_ROUTES.actorsList} className="header-link">
+            {pathname === '/' ? (
+              <li className="header-item">
+                <a href="#movies" className="header-link">
+                  <span className="header-text">Movies</span>
+                </a>
+              </li>
+            ) : (
+              <li className="header-item">
+                <Link to="/">
+                  <a href="#movies" className="header-link">
+                    <span className="header-text">Home</span>
+                  </a>
+                </Link>
+              </li>
+            )}
+            {pathname === '/' ? (
+              <li className="header-item">
+                <Link to={APP_ROUTES.actorsList} className="header-link">
+                  <span className="header-text">Actors</span>
+                </Link>
+              </li>
+            ) : (
+              <li className="header-item">
                 <span className="header-text">Actors</span>
-              </Link>
-            </li>
-            <li className="header-item logo-item">
-              <Link to="/#" className="header-link logo-link">
-                <div className="header-logo">
-                  <img src="./img/site-logo.png" alt="site logo" width="58" height="53"/>
-                </div>
-              </Link>
-            </li>
-            <li className="header-item">
-              <a href="#trailers" className="header-link">
-                <span className="header-text">Trailers</span>
-              </a>
-            </li>
+              </li>
+            )}
+            {pathname !== '/' ? (
+              <li className="header-item logo-item">
+                <Link to="/" className="header-link logo-link">
+                  <div className="header-logo">
+                    <img
+                      src="./img/site-logo.png"
+                      alt="site logo"
+                      width="58"
+                      height="53"
+                    />
+                  </div>
+                </Link>
+              </li>
+            ) : (
+              <li className="header-item logo-item">
+                <Link to="/" className="header-link logo-link">
+                  <div className="header-logo">
+                    <img
+                      src="./img/site-logo.png"
+                      alt="site logo"
+                      width="58"
+                      height="53"
+                    />
+                  </div>
+                </Link>
+              </li>
+            )}
+            {pathname === '/' && (
+              <li className="header-item">
+                <a href="#trailers" className="header-link">
+                  <span className="header-text">Trailers</span>
+                </a>
+              </li>
+            )}
           </ul>
         </div>
         <div className="search-block">
